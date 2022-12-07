43f8d3bf267de94387ba757005bdaeb91ffcc4ce
adriano328 <lucasadrianodias@gmail.com>
2022-12-06 22:57:38 -0400

    ajustes de tela

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IChild } from '../shared/interface/IChild';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  child: IChild = {} as IChild;
+  childId!: number;
+
+  constructor(
+    private router: ActivatedRoute
+  ) {
+    this.childId = parseInt(this.router.snapshot.paramMap.get('id')!);
+
+    this.child.foto = '/assets/menino1.jpg';
+    this.child.id = 1;
+    this.child.nome = 'Lucas Adriano Dias';
+    this.child.idade = 5;
+    this.child.sala = '5 a 10 Anos';
+    this.child.responsavel = 'Nahyara Amanda';
+    this.child.observacao = 'Intolerancia a lactóse'
+  }
 
   ngOnInit() {
+
   }
+
 
 }
